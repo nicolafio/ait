c38cf5e32b778757f50b9dca0b1ac521ecf16559
Nicola Fiori (JD342) <nicola@jd342.net>
2017-03-21 13:42:58 +0100

    Implement ability to save .xpi file directly in a specified location
        Makes updating the content of the installation easier by
        replacing the content automatically without the need to
        manually build the .xpi file, uninstall the older version and
        install the new one.

--- build.sh
+++ build.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
-build_file=arcthunderbird.xpi
-mkdir -p build
-rm -f build/$build_file
-cd source; zip -r ../build/$build_file .
-cd ..; zip build/$build_file license
-echo "done"
+
+build() {
+    echo building at $1
+    rm -f "$1"
+    cd source; zip -r "$1" .
+    cd ..; zip "$1" license
+    echo done building
+}
+
+case $1 in
+
+    --help)
+
+        echo "buid.sh contains logic used for development"
+        echo "bash build.sh           creates .xpi file in default build folder"
+        echo "bash build.sh --export  creates .xpi file in a specified location"
+        echo "bash build.sh --help    shows this help dialog"
+
+    ;;
+
+    --export)
+
+        export_target_file=.export-target
+
+        if [ ! -f $export_target_file ]
+        then
+            echo specify target .xpi file to be used:
+            read -e export_target || exit $?
+            export_target=`bash -c "echo $export_target"`
+            export_target=`realpath "$export_target"` || exit $?
+            echo "$export_target" >> "$export_target_file"
+            echo to reset the export target location, remove $export_target_file
+        else export_target=`cat "$export_target_file"` || exit $?
+        fi
+
+        build "$export_target"
+
+    ;;
+
+    *)
+
+        build_file=arcthunderbird.xpi
+
+        mkdir -p build
+        build `realpath "build/$build_file"`
+
+    ;;
+
+esac
+
 exit 0
