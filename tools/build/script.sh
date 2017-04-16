96cf5e016e4d79a18262cf444f91f7d037d51122
Nicola Fiori (JD342) <nicola@jd342.net>
2017-04-16 11:01:42 +0200

    Use more brief copyright headers and clean up & optimize icons

--- tools/build/script.sh
+++ tools/build/script.sh
@@ -2,21 +2,8 @@
 
 #  Copyright (c) 2017 Nicola Fiori (JD342)
 #
-#  This file is part of the Arc Integration for Thunderbird.
-#
-#  The Arc Integration for Thunderbird is free software: you can
-#  redistribute it and/or modify it under the terms of the
-#  GNU General Public License Version 3 as published by the
-#  Free Software Foundation.
-#
-#  The Arc Integration for Thunderbird is distributed in the hope
-#  that it will be useful, but WITHOUT ANY WARRANTY; without even the
-#  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#  See the GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with the Arc Integration for Thunderbird.
-#  If not, see <http://www.gnu.org/licenses/>.
+#  This file is part of the Arc Integration for Thunderbird, licensed under
+#  the terms of the GNU General Public License 3.0.
 
 main() {
 
