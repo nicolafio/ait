1b8151817187a42965eb2b6ed234ad9fc84d1fff
Nicola Fiori (JD342) <nicola@jd342.net>
2017-05-22 16:00:39 +0200

    Refactor style live reload tool addon
        Removes unnecessary block scoping

--- tools/style-livereload/client-addon/logic.js
+++ tools/style-livereload/client-addon/logic.js
@@ -6,130 +6,125 @@
  ~
  */
 
-{
+const host = 'localhost';
+const port = 8001;
+const address = `ws://${host}:${port}`;
+const retryDelay = 500;
+const events = new DocumentFragment();
+let css = null;
 
-    const host = 'localhost';
-    const port = 8001;
-    const address = `ws://${host}:${port}`;
-    const retryDelay = 500;
-    const events = new DocumentFragment();
-    let css = null;
+attemptConnection();
 
-    const main = () => {
+promiseInclusion('.ait-style').then(handleStyle);
 
-        attemptConnection();
-        promiseInclusion('.ait-style').then(handleStyle);
+promiseInclusion('.ait-icons').then((icons) => {
 
-        promiseInclusion('.ait-icons').then((icons) => {
+    const observer = new MutationObserver((records) => {
 
-            const observer = new MutationObserver((records) => {
+        records.forEach((record) => {
 
-                records.forEach((record) => {
-
-                    Array.from(record.addedNodes).forEach(handleIcon);
-
-                });
-
-            });
-
-            Array.from(icons.children).forEach(handleIcon);
-            observer.observe(icons, { childList: true });
+            [...record.addedNodes].forEach(handleIcon);
 
         });
 
-    };
+    });
 
-    /**
-     * @param {string} query
-     * @returns {Promise<HTMLElement>}
-     */
-    const promiseInclusion = (query) => new Promise((resolve) => {
+    [...icons.children].forEach(handleIcon);
+    observer.observe(icons, { childList: true });
 
-        const onFrame = () => {
+});
+
+/**
+ * @param   {string} query
+ * @returns {Promise<Element>}
+ */
+function promiseInclusion(query) {
+
+    return new Promise((resolve) => {
+
+        requestAnimationFrame(onFrame);
+
+        function onFrame() {
 
             const element = document.querySelector(query);
+
             if (element !== null) resolve(element);
             else requestAnimationFrame(onFrame);
 
-        };
-
-        requestAnimationFrame(onFrame);
+        }
 
     });
 
+}
+
+/**
+ * @param {HTMLObjectElement} icon
+ */
+function handleIcon(icon) {
+
+    requestAnimationFrame(onFrame);
+
+    function onFrame() {
+
+        const svgDocument = icon.contentDocument;
+
+        if (svgDocument !== null &&
+            svgDocument.firstElementChild !== null) {
+            requestAnimationFrame(onceDocumentIsReady);
+        }
+        else requestAnimationFrame(onFrame);
+
+    }
+
+    function onceDocumentIsReady() {
+
+        handleStyle(icon.contentDocument.querySelector('.ait-style'));
+
+    }
+
+}
+
+/**
+ * @param {HTMLStyleElement} style
+ */
+function handleStyle(style) {
+
+    if (css !== null) onStyleUpdate();
+    events.addEventListener('update', onStyleUpdate);
+
+    function onStyleUpdate() {
+
+        style.textContent = css;
+
+    }
+
+}
+
+function attemptConnection() {
+
+    const socket = new WebSocket(address);
+    socket.addEventListener('close', onceConnectionAborts);
+    socket.addEventListener('error', onceConnectionAborts);
+    socket.addEventListener('message', onMessage);
+
     /**
-     * @param {HTMLObjectElement} icon
+     * @param {MessageEvent} event
      */
-    const handleIcon = (icon) => {
+    function onMessage(event) {
 
-        const onFrame = () => {
+        css = atob(event.data);
+        events.dispatchEvent(new CustomEvent('update'));
 
-            const svgDocument = icon.contentDocument;
+    }
 
-            if (svgDocument !== null &&
-                svgDocument.firstElementChild !== null) {
-                requestAnimationFrame(onceDocumentIsReady);
-            }
-            else requestAnimationFrame(onFrame);
+    function onceConnectionAborts() {
 
-        };
+        socket.close();
+        socket.removeEventListener('close', onceConnectionAborts);
+        socket.removeEventListener('error', onceConnectionAborts);
+        socket.removeEventListener('message', onMessage);
+        setTimeout(attemptConnection, retryDelay);
 
-        const onceDocumentIsReady = () => {
-
-            const svgDocument = icon.contentDocument;
-
-            /** @type {HTMLStyleElement} */
-            const style = svgDocument.querySelector('.ait-style');
-
-            handleStyle(style);
-
-        };
-
-        requestAnimationFrame(onFrame);
-
-    };
-
-    /**
-     * @param {HTMLStyleElement} style
-     */
-    const handleStyle = (style) => {
-
-        const onStyleUpdate = () => { style.textContent = css; };
-
-        if (css !== null) onStyleUpdate();
-        events.addEventListener('update', onStyleUpdate);
-
-    };
-
-    const attemptConnection = () => {
-
-        /**
-         * @param {MessageEvent} event
-         */
-        const onMessage = (event) => {
-
-            css = atob(event.data);
-            events.dispatchEvent(new CustomEvent('update'));
-
-        };
-
-        const onceConnectionAborts = () => {
-
-            socket.close();
-            socket.removeEventListener('close', onceConnectionAborts);
-            socket.removeEventListener('error', onceConnectionAborts);
-            socket.removeEventListener('message', onMessage);
-            setTimeout(attemptConnection, retryDelay);
-
-        };
-
-        const socket = new WebSocket(address);
-        socket.addEventListener('close', onceConnectionAborts);
-        socket.addEventListener('error', onceConnectionAborts);
-        socket.addEventListener('message', onMessage);
-
-    };
-
-    main();
+    }
 
 }
